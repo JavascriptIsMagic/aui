04852fd52bf7cf9ce50e1bd997d610aadfb06ec5
Brian <javascriptismagic@gmail.com>
2014-12-23 00:48:03 -0600

    examples in segments

--- index.jsx
+++ index.jsx
@@ -2,21 +2,33 @@ var
 	React = window.React,
 	Aui = window.Aui;
 
+var Example = React.createClass({
+	render: function () {
+		return (
+			<Aui>
+				<div ui item>
+					<div ui segment>
+						<a ui header href={"http://jsfiddle.net/" + this.props.fiddle + "/"}>{this.props.title}</a>
+						<div ui description>
+							<p multiline>{this.props.children}</p>
+							<p><iframe width="100%" height="300" src={"http://jsfiddle.net/" + this.props.fiddle + "/embedded/"} allowfullscreen="allowfullscreen" frameborder="0"></iframe></p>
+						</div>
+					</div>
+				</div>
+			</Aui>
+		);
+	}
+});
+
 var Page = React.createClass({
 	render: function () {
 		return (
 			<Aui>
 				<div ui basic segment>
+					<div ui divider/>
 					<h1 ui dividing header>Examples</h1>
 					<div ui items>
-						<div ui item>
-							<a ui header href="http://jsfiddle.net/reactjs/69z2wepo/">
-								React's Hello World
-							</a>
-							<div ui description>
-								<iframe width="100%" height="300" src="http://jsfiddle.net/reactjs/69z2wepo/embedded/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>
-							</div>
-						</div>
+						<Example title="Basic React Example" fiddle="reactjs/69z2wepo" />
 					</div>
 				</div>
 			</Aui>

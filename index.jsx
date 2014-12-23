3a2e080e299ea62aca2d8197bae971bdcb99092f
Brian <javascriptismagic@gmail.com>
2014-12-22 23:56:00 -0600

    render examples

---
+++ index.jsx
@@ -0,0 +1,35 @@
+var
+	React = window.React,
+	Aui = window.Aui;
+
+var Page = React.createClass({
+	render: function () {
+		return (
+			<Aui>
+				<div ui basic segment>
+					<h1 ui dividing header>Examples</h1>
+					<div ui items>
+						<div item>
+							<a header href="http://jsfiddle.net/reactjs/69z2wepo/">
+								React's Hello World
+							</a>
+							<div description>
+								<iframe width="100%" height="300" src="http://jsfiddle.net/reactjs/69z2wepo/embedded/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>
+							</div>
+						</div>
+					</div>
+				</div>
+			</Aui>
+		);
+	}
+});
+
+React.render((
+	<Aui>
+		<div ui page grid>
+			<div column>
+				<Page/>
+			</div>
+		</div>
+	</Aui>
+), document.body);

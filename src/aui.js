f723055aae279b11b6c4af197bf8acbaeb045ebe
Brian <javascriptismagic@gmail.com>
2014-12-29 14:59:45 -0600

    more consistent form event handlers
    including normalizing dropdown interaction with form onInput/onChange
    event handlers, and including them in the form onSubmit event

--- src/aui.js
+++ src/aui.js
@@ -102,57 +102,120 @@ var Semantic = React.createClass({
   propTypes: { children: React.PropTypes.element.isRequired },
 	getInitialState: function () { return { formData: {} } },
 	onFormInput: function (event) {
-		var formData = this.state.formData;
-		formData[event.target.name] = event.target.value;
-		this.setState({ formData: formData });
+		var target = event.target;
+		setTimeout(function () {
+			var formData = this.state.formData;
+			formData[target.name] = target.value;
+			console.log(target.name, target.value);
+			this.setState({ formData: formData });
+		}.bind(this));
+	},
+	componentWillMount: function () {
+		var onInput = this.props.children.props.onInput,
+			onFormInput = this.onFormInput;
+		this.bothFormOnInput = function (event) {
+			onFormInput.apply(this, arguments);
+			if (onInput) { onInput.apply(this, arguments); }
+		}
 	},
 	render: function () {
 		if (this.props.children.props.form) {
-			var onFormInput = this.props.children.props.onInput,
-				componentOnFormInput = this.onFormInput;
 			return cloneWithProps(this.props.children, {
-				onInput: function (event) {
-					componentOnFormInput.apply(this, arguments);
-					if (onFormInput) { onFormInput.apply(this, arguments); }
-				}
+				onInput: this.bothFormOnInput
 			});
 		}
 		return this.props.children;
 	},
 	applySettings: function (settings) {
-		var props = this.props.children.props,
+		var
+			self = this,
+			props = this.props.children.props,
 			element = jQuery(this.getDOMNode()),
-			formData = this.state.formData;
+			bothFormOnInput = this.bothFormOnInput;
+		function noop() {};
+		function AuiSyntheticSyntheticEvent($target) {
+			var target = (
+				$target.filter('select,input')[0] ||
+				$target.parents('.dropdown:first').find('select,input')[0] ||
+				$target[0] ||
+				element[0]);
+			this.type = 'input';
+			this.timeStamp = Date.now();
+			this.target = target;
+			this.preventDefault =
+			this.isPersistent =
+			this.isDefaultPrevented =
+			this.isPropagationStopped =
+			this.destructor =
+			this.spotPropagation = noop;
+		};
+		if (props.name) { element.attr('data-name', props.name); }
 		props.className.split(' ')
 			.filter(function (moduleType) { return moduleSearch.test(moduleType); })
 			.map(function (moduleType) {
 				var settings = props[moduleType];
+				if (!settings) { return; }
 				settings = Array.isArray(settings) ? settings : [settings];
 				var options = settings[settings.length-1] = settings[settings.length-1] || {};
+				if (options === true) {
+					options = settings[settings.length-1] = {};
+				}
 				if (moduleType === 'form') {
 					if (!settings[1]) { settings[1] = {}; }
 					if (props.onSubmit) {
 						settings[1].onSuccess = function (event) {
-							event.data = formData;
+							event.data = self.state.formData;
 							props.onSubmit.apply(this, arguments);
 						};
 					}
 					options = settings[1];
-				}
-				Object.keys(props)
-					.filter(function (key) { return /^on[A-Z]/.test(key); })
-					.map(function (callback) {
-						options[callback] = props[callback];
+					setTimeout(function () {
+						element.find('[data-name]').each(function (_, input) {
+							var
+								$input = jQuery(input),
+								data = self.state.formData;
+							data[$input.attr('data-name')] = $input.val();
+						});
 					});
+				}
+				if (typeof options === 'object') {
+					var onChange = options.onChange;
+					Object.keys(props)
+						.filter(function (key) { return /^on[A-Z]/.test(key); })
+						.map(function (callback) {
+							if (!/^on(Input|Change)$/.test(callback)) {
+								options[callback] = props[callback];
+							}
+							console.log(callback, props[callback]);
+							element.data(callback, props[callback]);
+						});
+					if (moduleType === 'form') { element.data('onInput', bothFormOnInput); }
+					options.onChange = function (_, value, $target) {
+						if (onChange) { onChange.apply(this, arguments); }
+						if (props.onInput) { props.onInput.call(this, new AuiSyntheticSyntheticEvent($target)); }
+						if (props.onChange) { props.onChange.call(this, new AuiSyntheticSyntheticEvent($target)); }
+						var form = $target.parents('.form:first');
+						if (form[0]) {
+							if (form.data('onChange')) { form.data('onChange').call(this, new AuiSyntheticSyntheticEvent($target)); }
+							if (form.data('onInput')) { form.data('onInput').call(this, new AuiSyntheticSyntheticEvent($target)); }
+						}
+					};
+				}
 				element[moduleType].apply(element, settings);
+				if (props.name) {
+					setTimeout(function () {
+						var formOnInput = element.parents('.form:first').data('onInput');
+						if (formOnInput) {
+							formOnInput(new AuiSyntheticSyntheticEvent(element));
+						}
+					});
+				}
 			});
 	},
 	componentDidMount: function () {
 		this.applySettings();
 	},
 	componentWillUnmount: function () {
-		var props = this.props.children.props,
-			element = jQuery(this.getDOMNode());
 		jQuery(this.getDOMNode()).remove();
 	},
 });

2b58bd6100a37616ec70109b159f317fba57ce2e
Coffeecorn <javascriptismagic@gmail.com>
2015-09-25 16:20:45 -0500

    fix externals from plain .js

--- externals.js
+++ externals.js
@@ -1,4 +1,5 @@
-window.jQuery = require 'jquery'
-require 'semantic-ui/dist/semantic.js'
-window.React = require 'react'
-module.exports = require './dist/aui.js'
+'use strict';
+window.jQuery = require('jquery');
+require('semantic-ui/dist/semantic.js');
+window.React = require('react/addons');
+module.exports = require('./dist/aui.js');
